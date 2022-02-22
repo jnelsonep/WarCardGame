38accd8bd390c04f5b6f5fd3290fbef9d1a318a3
Jenn Nelson <jnelson@edenpr.org>
2022-02-22 09:42:25 -0600

    uploaded files

---
+++ dudeeee/dudeeeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  dudeeeeApp.swift
+//  dudeeee
+//
+//  Created by Jennifer Nelson on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct dudeeeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
